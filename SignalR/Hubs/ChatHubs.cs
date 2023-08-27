35fb39d54822e569bbe0000b673c4b4e3ad317d3
alirezang7575@gmail.com <alirezang7575@gmail.com>
2023-08-27 22:40:53 +0430

    this is chatroom for Lreanig SignalR

---
+++ SignalR/Hubs/ChatHubs.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs;
+
+public class ChatHubs:Hub
+{
+    public async Task Sendmessage(string username, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", username, message);
+    }
+}
---
+++ SignalR/Program.cs
@@ -0,0 +1,27 @@
+using SignalR.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapHub<ChatHubs>("/chatHub");
+
+app.Run();
